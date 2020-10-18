5823b15da404d91dfe01968d4f6c279996fe8f7a
Nerio <neriogonzalez@gmail.com>
2020-10-18 20:27:36 -0300

    adding html dinamic content

--- email_sender.py
+++ email_sender.py
@@ -1,11 +1,19 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path("index.html").read_text())
+
+
+
 email = EmailMessage()
 email['from']='Nerio Gonzalez'
 email['to']='neriogonzalez@gmail.com'
 email['subject']='correo de prueba desde python'
 
-email.set_content('Este es mi primer email desde python')
+email.set_content(html.substitute(name = 'Tintin'), 'html')
+#email.set_content('Este es mi primer email desde python')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
