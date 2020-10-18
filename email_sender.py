ca0ea7df159504cf41793c09a9d70c5e8d6123e6
Nerio <neriogonzalez@gmail.com>
2020-10-18 19:00:03 -0300

    first commit

---
+++ email_sender.py
@@ -0,0 +1,15 @@
+import smtplib
+from email.message import EmailMessage
+email = EmailMessage()
+email['from']='Nerio Gonzalez'
+email['to']='neriogonzalez@gmail.com'
+email['subject']='correo de prueba desde python'
+
+email.set_content('Este es mi primer email desde python')
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('dummyneriogonzalez@gmail.com', 'Dummy1248357')
+    smtp.send_message(email)
+    print('Enviado')
+
